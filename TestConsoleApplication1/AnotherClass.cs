bd2f1374eedf5d2862965fc6af43a7db52ec8858
gpehartz <gpehartz@gmail.com>
2015-03-10 08:51:54 +0000

    Initial add

---
+++ TestConsoleApplication1/AnotherClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication1
+{
+    public class AnotherClass
+    {
+        private void Test()
+        {
+            var d = new Event3();
+            var e = new Event4();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void ApplyEvent(Event @event)
+        {
+
+        }
+    }
+}
---
+++ TestConsoleApplication1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace TestConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new Event();
+            var c = new Event2();
+        }
+
+        private void Test()
+        {
+            var d = new Event3();
+            var e = new Event4();
+
+            var x = new Event3();
+
+
+            var b = new Event();
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void ApplyEvent(Event4 @event)
+        {
+
+        }
+
+        private void ApplyEvent(Event5 @event)
+        {
+
+        }
+
+        private void ApplyEvent(Event6 @event)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void ApplyEvent(Event2 @event)
+        {
+
+        }
+
+        private void ApplyEvent(Event3 @event)
+        {
+
+        }
+    }
+
+    public class Event : BaseEvent
+    {
+    }
+
+    public class Event2
+    {
+    }
+
+    public class Event3 : BaseEvent
+    {
+    }
+
+    public class Event4
+    {
+    }
+
+    public class Event5
+    {
+    }
+
+    public class Event6
+    {
+    }
+
+    public class Event7
+    {
+    }
+}
